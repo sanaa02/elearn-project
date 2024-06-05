dbf34b8f1f71be2bc300ed106218dd013ddca106
sanaa02 <s.bensaid@esi-sba.dz>
2024-06-05 10:29:47 +0100

    login linked

--- Front-end/vite-project/src/Components/pages/LoginModal.jsx
+++ Front-end/vite-project/src/Components/pages/LoginModal.jsx
@@ -1,10 +1,64 @@
-
-import { Box, Typography, Button, Modal } from '@mui/material';
-import model from '../../assets/Model 2.png';
-import './WelcomePage.css' 
+import React, { useState } from "react";
+import { Box, Typography, Button, Modal } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import model from "../../assets/Model 2.png";
+import "./WelcomePage.css";
+import jwt_decode from "jwt-decode";
 
 function LoginModal({ open, handleClose }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
+    const loginUser = async (email, password) => {
+      console.log(email, password);
+      const response = await fetch("http://127.0.0.1:8000/account/token/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      console.log(email, password);
+      const data = await response.json();
+      console.log(data);
+
+      if (response.status == 200) {
+        console.log("Logged in successfully");
+        setAuthTokens(data);
+        const user = jwt_decode(data.access);
+        setUser(user);
+        // setUser(jwt_decode(data.access));
+        localStorage.setItem("authTokens", JSON.stringify(data));
+        if (user.is_superuser) {
+          navigate("/admin");
+        } else if (user.is_student) {
+          navigate("/StudentPage");
+        } else if (user.is_professor) {
+          navigate("/Enseignant");
+        }
+        //history.push("/")
+      } else {
+        console.log(response.status);
+        console.log("there was a server issue");
+      } }
+  
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      // Implement form submission logic here.
+      console.log("Email:", email);
+      console.log("Message:", password);
+
+      loginUser(email, password);
+
+      setEmail("");
+      setPassword("");
+    };
+  // };
 
   return (
     <Modal
@@ -17,12 +71,28 @@ function LoginModal({ open, handleClose }) {
         <img src={model} alt="" className="logo-esi" />
         <Typography className="Moddal-title"> Connexion</Typography>
         <form className="login">
-          <input type="text" placeholder="Username" required />
-          <input type="password" placeholder="Password" required />
+          <input
+            type="text"
+            placeholder="Username"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </form>
         <Box className="button-box">
-          <Button className="button">confirmer</Button>
-          <Button className="button" onClick={handleClose}>Annuler</Button>
+          <Button className="button" onClick={handleSubmit}>
+            confirmer
+          </Button>
+          <Button className="button" onClick={handleClose}>
+            Annuler
+          </Button>
         </Box>
       </Box>
     </Modal>
